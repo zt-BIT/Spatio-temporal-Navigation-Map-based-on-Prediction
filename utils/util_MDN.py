5a53ef70995e06a46277a6c784f6296fb1d6331d
zt_bit <31545415+zt-BIT@users.noreply.github.com>
2020-06-16 09:33:11 +0800

    Update util_MDN.py

--- utils/util_MDN.py
+++ utils/util_MDN.py
@@ -20,18 +20,5 @@ def tf_2d_normal(x1, x2, mu1, mu2, s1, s2, rho):
     px1x2 = tf.div(result, denom)
     return px1x2
 
-def tf_1d_normal(x3,mu3,s3):
-    """ 3D normal distribution Under assumption that x3 is uncorrelated with x1 and x2
-    input
-    - x,mu: input vectors
-    - s1,s2,s3: standard deviances over x1 and x2 and x3
-    - rho: correlation coefficient in x1-x2 plane
-    """
-    norm3 = tf.subtract(x3, mu3)
-    z = tf.square(tf.div(norm3, s3))
-    result = tf.exp(tf.div(-z,2))
-    denom = 2.0*np.pi*s3
-    px3 = tf.div(result, denom)  #probability in x3 dimension
-    return px3
 
  
